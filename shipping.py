3600a0e0dc99b3b8fb31845a3b5105f43dfe0b6c
Abena Adu <abeanadu@gmail.com>
2024-02-27 22:03:09 -0500

    made code more concise

--- shipping.py
+++ shipping.py
@@ -10,20 +10,16 @@ error_message = "ERROR: Invalid weight"
 #this is the flat charge amount for ground shipping packages
 #in dollars 
 flat_charge_ground_shipping = 20
-greeting_for_ground_shipping_cost = "Amount Due for Ground Shipping: "
+greeting_for_ground_shipping_cost = "Amount Due for Ground Shipping:"
 #Conditional for how much package will cost based on weight for ground shipping
 if weight > 0 and weight <= 2:
-  print(greeting_for_ground_shipping_cost)
-  print(flat_charge_ground_shipping + (weight * 1.50))
+  print(greeting_for_ground_shipping_cost, flat_charge_ground_shipping + (weight * 1.50))
 elif weight > 2 and weight <= 6:
-  print(greeting_for_ground_shipping_cost)
-  print(flat_charge_ground_shipping + (weight * 3.00))
+  print(greeting_for_ground_shipping_cost, flat_charge_ground_shipping + (weight * 3.00))
 elif weight > 6 and weight <= 10:
-  print(greeting_for_ground_shipping_cost)
-  print(flat_charge_ground_shipping + (weight * 4.00))
+  print(greeting_for_ground_shipping_cost, flat_charge_ground_shipping + (weight * 4.00))
 elif weight > 10:
-  print(greeting_for_ground_shipping_cost)
-  print(flat_charge_ground_shipping + (weight * 4.75))
+  print(greeting_for_ground_shipping_cost, flat_charge_ground_shipping + (weight * 4.75))
 else:
   print(error_message)
 
@@ -31,28 +27,22 @@ else:
 #flat charge for ground shipping premium
 #in dollars
 flat_charge_ground_shipping_premium = 125
-greeting_for_ground_premium_shipping_cost = "Amount Due for Ground Shipping Premium: "
+greeting_for_ground_premium_shipping_cost = "Amount Due for Ground Shipping Premium:"
 if weight > 0:
-  print(greeting_for_ground_premium_shipping_cost)
-  print(flat_charge_ground_shipping_premium)
+  print(greeting_for_ground_premium_shipping_cost, flat_charge_ground_shipping_premium)
 else:
   print(error_message)
 
-
 #Drone Shipping
-greeting_for_drone_shipping_cost = "Amount Due for Drone Shipping: "
+greeting_for_drone_shipping_cost = "Amount Due for Drone Shipping:"
 #Conditional for how much package will cost based on weight for drone shipping
 if weight > 0 and weight <= 2:
-  print(greeting_for_drone_shipping_cost)
-  print(weight * 4.50)
+  print(greeting_for_drone_shipping_cost, weight * 4.50)
 elif weight > 2 and weight <= 6:
-  print(greeting_for_drone_shipping_cost)
-  print(weight * 9.00)
+  print(greeting_for_drone_shipping_cost, weight * 9.00)
 elif weight > 6 and weight <= 10:
-  print(greeting_for_drone_shipping_cost)
-  print(weight * 12)
+  print(greeting_for_drone_shipping_cost, weight * 12)
 elif weight > 10:
-  print(greeting_for_drone_shipping_cost)
-  print(weight * 14.25)
+  print(greeting_for_drone_shipping_cost, weight * 14.25)
 else:
   print(error_message)
