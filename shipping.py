2f4df236266d6a3fd56a159052a7efee0b4bd955
Abena Adu <abeanadu@gmail.com>
2024-02-27 19:28:31 -0500

    Drone Shipping and added error message

--- shipping.py
+++ shipping.py
@@ -1,6 +1,9 @@
 #Weight of package
 #weight is in lbs
-weight = 1
+weight = 50
+
+#Error message for if user enters weight below zero
+error_message = "ERROR: Invalid weight"
 
 #Ground shipping
 
@@ -22,10 +25,34 @@ elif weight > 10:
   print(greeting_for_ground_shipping_cost)
   print(flat_charge_ground_shipping + (weight * 4.75))
 else:
-  print("ERROR")
+  print(error_message)
 
 #Ground Shipping Premium
 #flat charge for ground shipping premium
+#this is in dollars
 flat_charge_ground_shipping_premium = 125
-print("Amount Due for Ground Shipping Premium: ")
-print(flat_charge_ground_shipping_premium)
+greeting_for_ground_premium_shipping_cost = "Amount Due for Ground Shipping Premium: "
+if weight > 0:
+  print(greeting_for_ground_premium_shipping_cost)
+  print(flat_charge_ground_shipping_premium)
+else:
+  print(error_message)
+
+
+#Drone Shipping
+greeting_for_drone_shipping_cost = "Amount Due for Drone Shipping: "
+#Conditional for how much package will cost based on weight for drone shipping
+if weight > 0 and weight <= 2:
+  print(greeting_for_drone_shipping_cost)
+  print(weight * 4.50)
+elif weight > 2 and weight <= 6:
+  print(greeting_for_drone_shipping_cost)
+  print(weight * 9.00)
+elif weight > 6 and weight <= 10:
+  print(greeting_for_drone_shipping_cost)
+  print(weight * 12)
+elif weight > 10:
+  print(greeting_for_drone_shipping_cost)
+  print(weight * 14.25)
+else:
+  print(error_message)
